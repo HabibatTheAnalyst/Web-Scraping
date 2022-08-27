6684c1d19f5861eb9d368a26086cf27e62b179c8
Habibat <habibatjimoh9@gmail.com>
2022-08-27 14:34:38 +0100

    update

--- 2. flights.py
+++ 2. flights.py
@@ -5,8 +5,4 @@ import requests, openpyxl # to open the scrapped data in excel
 url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
 source = requests.get(url)
 soup = BeautifulSoup(source.text, 'lxml')
-<<<<<<< HEAD
 movies = soup.find('tbody', class_ = 'lister-list').find_all('tr')
-=======
-movies = soup.find('tbody', class_ = 'lister-list').find_all('tr')
->>>>>>> 6e69f629d17150e030166953cbf001256825fbe9
