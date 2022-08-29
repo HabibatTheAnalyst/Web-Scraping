76e0df23f72436828f3ff2c4632c18f32b3828dc
Habibat <habibatjimoh9@gmail.com>
2022-08-29 19:07:40 +0100

    update

--- 2. flights.py
+++ 2. flights.py
@@ -1,15 +1,8 @@
-from requests_html import HTMLSession
+import requests
 from bs4 import BeautifulSoup
 
-session = HTMLSession()
-url = 'https://www.youtube.com/c/JohnWatsonRooney/videos'
-source = session.get(url)
-soup = BeautifulSoup(source.text, 'lxml')
-videos = soup.find('div', class_ = 'style-scope ytd-grid-renderer')
+url = 'https://www.pararius.com/apartments/amsterdam?ac=1'
+source = requests.get(url)
+soup = BeautifulSoup(source.text, 'html.parser')
 
-for video in videos:
-
-    name = video.find('h3', class_ = 'style-scope ytd-grid-renderer').a.text
-    print(name)
-
-    
+print(soup)
