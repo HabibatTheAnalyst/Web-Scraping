1f180bb7e34d29190a473536e138e44c35f86a3d
Habibat <habibatjimoh9@gmail.com>
2022-08-30 00:46:40 +0100

    code

--- 2. flights.py
+++ 2. flights.py
@@ -1,8 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.pararius.com/apartments/amsterdam?ac=1'
-source = requests.get(url)
-soup = BeautifulSoup(source.text, 'html.parser')
+for x in range(1,19):
+    url = 'https://www.pararius.com/apartments/amsterdam?ac='
+    source = requests.get(url + str(x))
+    soup = BeautifulSoup(source.text, 'lxml')
+    apartments = soup.find('ul', class_ = 'search-list').find_all('section')
+        
+    for apartment in apartments:
+        name = apartment.find('a', class_ = 'listing-search-item__link--title').text
+        price = apartment.find('div', class_ = 'listing-search-item__price').text
+        location = apartment.find('div', class_ = 'listing-search-item__sub-title').text
+        area = apartment.find('li', class_ = 'illustrated-features__item--surface-area').text
+        rooms = apartment.find('li', class_ = 'illustrated-features__item--number-of-rooms').text
 
-print(soup)
+print(name, price, location, area, rooms)
