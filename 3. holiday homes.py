07195898d97bd31152150a9519999b77be1d909a
Habibat <habibatjimoh9@gmail.com>
2022-08-31 22:41:18 +0100

    code

--- 3. holiday homes.py
+++ 3. holiday homes.py
@@ -1,11 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-for x in range(1,10):
-    url = 'https://www.brittany-ferries.co.uk/holidays/search?vt=0&s=ASC&p='
-    source = requests.get(url + str(x))
-    soup = BeautifulSoup(source.text, 'lxml')
-    homes = soup.find('div', class_ = 'product-grid grid').find_all('dd-product-card')
+#for x in range(1,30):
+url = 'https://www.brittany-ferries.co.uk/holidays/search?vt=0&s=ASC&p='
+source = requests.get(url) # + str(x))
+soup = BeautifulSoup(source.text, 'lxml')
+homes = soup.find('section', class_ = 'ng-star-inserted').find_all('dd-product-card')
 
-    for home in homes:
-        name = home.h3.h2.text
+print(len(homes))
+'''
+for home in homes:
+    name = home.find('_ngcontent-serverapp-c290', class_ = 'ng-star-inserted')
+    print(name)
+'''
