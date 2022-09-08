1b050551dd5cec7a3ed97a3c11e307ffe0092bc2
Habibat <habibatjimoh9@gmail.com>
2022-09-08 18:59:03 +0100

    pretty.code

--- 2. apartments.py
+++ 2. apartments.py
@@ -2,6 +2,7 @@ from wsgiref import headers
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import time
 
 headers = {'User-Agent':  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'}
 
@@ -14,18 +15,19 @@ def getPretty(tag, page):
     dress = soup.find_all('div', class_ = 'category-product')
     for item in dress:
         tag = tag
-        title = item.find('h2', class_ = 'product-title').text.strip()
+        name = item.find('h2', class_ = 'product-title').text.strip()
         price = item.find('span', class_ = 'price').text.strip()
         link = item.find('a', class_ = 'product-url')['href']
 
-        pretty = {'tag': tag, 'title': title, 'price': price, 'link': link}
+        pretty = {'tag': tag, 'name': name, 'price': price, 'link': link}
         prettylist.append(pretty)
     return
 
-for x in range(1,3):
+for x in range(1,54):
     getPretty('denim-dresses', x)
     getPretty('mini-dresses', x)
-    
+time.sleep(5)
+
 df = pd.DataFrame(prettylist)
-df.to_excel('PrettyLittleThings.xlsx')
+df.to_excel('PrettyLittleThings.xlsx', index = False)
 print('Finish')
