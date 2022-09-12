b12e3ae588a558ee098ad276d0dec87f13fc98ac
Habibat <habibatjimoh9@gmail.com>
2022-09-12 22:32:38 +0100

    code

--- 4. Nigerian Stock Exchange Share Prices.py
+++ 4. Nigerian Stock Exchange Share Prices.py
@@ -1,28 +1,18 @@
-<<<<<<< HEAD
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-import time
 
 headers = {'User-Agent':  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'}
 
-booklist = []
-
-url = 'https://www.gutenberg.org/browse/scores/top'
+url = 'https://africanfinancials.com/nigerian-stock-exchange-share-prices/'
 source = requests.get(url, headers = headers)
 soup = BeautifulSoup(source.text, 'html.parser')
-books = soup.find('ol').find_all('li')
+shares = soup.find('tbody', class_ = 'wpv-loop').find_all('tr')
 
-for book in books:
-    title = book.find('a').text
+# print(source)
+# print(soup.title.text)
+# Nigerian Stock Exchange Share Prices - AfricanFinancials
 
-    list = {'title': title}
-    booklist.append(list)
+for stock in shares:
+    company = stock.find('td', class_ = 'dtr-control')
 
-time.sleep(5)
-df = pd.DataFrame(booklist)
-df.to_excel('Top 100 Books.xlsx', index = False)
-print('Finish')
-=======
-
->>>>>>> b86d4cd4a3c51f476232bc83e08b9360373b9a3e
+print(company)
