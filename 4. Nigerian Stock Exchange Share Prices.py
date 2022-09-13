b6a84a40b67bff423c932660dcc25fd5f1d9e11e
Habibat <habibatjimoh9@gmail.com>
2022-09-13 11:18:19 +0100

    code

--- 4. Nigerian Stock Exchange Share Prices.py
+++ 4. Nigerian Stock Exchange Share Prices.py
@@ -1,7 +1,12 @@
+from xml.dom.minidom import Attr
 import requests
 from bs4 import BeautifulSoup
+import time
+import pandas as pd
 
 headers = {'User-Agent':  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'}
+
+stocklist = []
 
 url = 'https://africanfinancials.com/nigerian-stock-exchange-share-prices/'
 source = requests.get(url, headers = headers)
@@ -13,8 +18,13 @@ shares = soup.find('tbody', class_ = 'wpv-loop').find_all('tr')
 # Nigerian Stock Exchange Share Prices - AfricanFinancials
 
 for stock in shares:
-    company = stock.get('td')
-    #price = stock.find('td', class_ = 'numeric').text
-    #prcntchange = stock.find('td', class_ = 'sorting_1')
+    company = stock.find('td').text
+    price = stock.find('td', class_ = 'numeric').text
 
-    print(company)
+    share = {'company':company, 'price':price}
+    stocklist.append(share)
+
+time.sleep(5)
+df = pd.DataFrame(stocklist)
+df.to_excel('Nigerian Stock Exchange.xlsx', index = False)
+print('Finish')
